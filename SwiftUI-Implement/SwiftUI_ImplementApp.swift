5ec161679be8ec539a77f0243738d5cb51bc67ad
ricardo0441 <ricardo0441@gmail.com>
2023-10-16 09:40:45 -0600

    Initial Commit

---
+++ SwiftUI-Implement/SwiftUI_ImplementApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ImplementApp.swift
+//  SwiftUI-Implement
+//
+//  Created by Ricardo Rodriguez on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ImplementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
